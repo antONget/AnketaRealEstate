61d331c0d56250fb5860cf146c3289635f23796b
antONget <a.l.ponomare@mail.ru>
2024-10-25 20:51:36 +0300

    init

---
+++ handlers/user_handler.py
@@ -0,0 +1,381 @@
+from aiogram import Router, F, Bot
+from aiogram.filters import CommandStart, StateFilter, or_f
+from aiogram.types import Message, CallbackQuery, ReplyKeyboardRemove
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
+
+
+from keyboards import user_keyboards as kb
+from config_data.config import Config, load_config
+from database import requests as rq
+from utils.error_handling import error_handler
+from filter.filter import validate_russian_phone_number
+from utils.send_admins import send_message_admins
+import logging
+router = Router()
+# Загружаем конфиг в переменную config
+config: Config = load_config()
+
+
+class User(StatesGroup):
+    input_other = State()
+    type_wall = State()
+    phone = State()
+
+
+@router.message(CommandStart())
+@error_handler
+async def process_start_command_user(message: Message, state: FSMContext, bot: Bot) -> None:
+    """
+    Пользовательский режим запускается если, пользователь ввел команду /start
+    1. Добавляем пользователя в БД если его еще нет в ней
+    :param message:
+    :param state:
+    :param bot:
+    :return:
+    """
+    logging.info(f'process_start_command_user: {message.chat.id}')
+    await state.set_state(state=None)
+    await state.clear()
+    if message.from_user.username:
+        username = message.from_user.username
+    else:
+        username = 'username'
+    await rq.add_user(tg_id=message.chat.id,
+                      data={"tg_id": message.chat.id, "username": username})
+    await message.answer(text=f'Добро пожаловать!',
+                         reply_markup=kb.keyboards_start_user())
+    await message.answer(text=f'Что вы хотите?',
+                         reply_markup=kb.keyboard_bay_sell())
+
+
+@router.message(F.text == '🏠 Главное меню')
+async def main_menu(message: Message, state: FSMContext, bot: Bot):
+    """
+    Переход к главному окну
+    :param message:
+    :param state:
+    :param bot:
+    :return:
+    """
+    logging.info(f'process_start_command_user: {message.chat.id}')
+    await state.set_state(state=None)
+    await state.clear()
+    await process_start_command_user(message=message, state=state, bot=bot)
+
+
+@router.callback_query(F.data.startswith("estate_"))
+@error_handler
+async def type_real_estate(callback: CallbackQuery, state: FSMContext, bot: Bot):
+    """
+    Выбор типа недвижимости [Земельный участок, Дом, Квартира]
+    :param callback: [estate_bay, estate_sell]
+    :param state:
+    :param bot:
+    :return:
+    """
+    logging.info(f'type_real_estate {callback.message.chat.id}')
+    answer = callback.data.split('_')[-1]
+    await state.update_data(payment=answer)
+    await callback.message.edit_text(text=f'Какой тип недвижимости Вы хотите продать?',
+                                     reply_markup=kb.keyboard_type_estate_real())
+    await callback.answer()
+
+
+@router.callback_query(F.data.startswith("type_estate_"))
+@error_handler
+async def type_real_estate(callback: CallbackQuery, state: FSMContext, bot: Bot):
+    """
+    Разветвление в зависимости от типа недвижимости
+    :param callback: [type_estate_land, type_estate_house, type_estate_flat]
+    :param state:
+    :param bot:
+    :return:
+    """
+    logging.info(f'type_real_estate {callback.message.chat.id}')
+    answer = callback.data.split('_')[-1]
+    await state.update_data(type=answer)
+    if answer == 'land':
+        await bot.delete_message(chat_id=callback.message.chat.id,
+                                 message_id=callback.message.message_id)
+        await callback.message.answer(text=f'Какое направление интересует?',
+                                      reply_markup=kb.keyboard_type_land())
+        await state.update_data(type_e="Земельный участок")
+    elif answer == 'house':
+        await callback.message.edit_text(text=f'Есть ли у Вас земельный участок',
+                                         reply_markup=kb.keyboard_type_estate_house())
+        await state.update_data(type_e="Дом")
+    elif answer == 'flat':
+        await callback.message.answer(text=f'Оставьте номер телефона наш специалист Вам перезвонит',
+                                      reply_markup=kb.keyboard_phone())
+        await state.update_data(type_e="Квартира")
+        await state.set_state(User.phone)
+    await callback.answer()
+
+
+@router.callback_query(F.data.startswith('type_house'))
+@error_handler
+async def process_house(callback: CallbackQuery, state: FSMContext, bot: Bot):
+    """
+    Получаем сведения об участке для дома и запрашиваем направление
+    :param callback: type_house_yes
+    :param state:
+    :param bot:
+    :return:
+    """
+    logging.info(f'type_real_estate {callback.message.chat.id}')
+    answer = callback.data.split('_')[-1]
+    await state.update_data(land=answer)
+    await bot.delete_message(chat_id=callback.message.chat.id,
+                             message_id=callback.message.message_id)
+    await callback.message.answer(text=f'Какое направление интересует?',
+                                  reply_markup=kb.keyboard_type_land())
+    await callback.answer()
+
+
+@router.message(F.text == 'Другой вариант')
+@error_handler
+async def process_input_other(message: Message, state: FSMContext, bot: Bot):
+    """
+    Ожидаем ввод пользователя
+    :param message:
+    :param state:
+    :param bot:
+    :return:
+    """
+    logging.info(f'process_start_command_user: {message.chat.id}')
+    await message.answer(text=f'Напишите ваш вариант?')
+    await state.set_state(User.input_other)
+
+
+@router.message(lambda message: message.text in ['Север (Выборгский, Приозерский, Всеволожский р-н)',
+                                                 'ЮГ (Гатчинский, Ломоносовский, Тосненский, Красносельский)',
+                                                 'Юго-восток (Кировский, Всеволожский)',
+                                                 'Только в границе СПБ'])
+@router.message(StateFilter(User.input_other))
+@error_handler
+async def get_input_other(message: Message, state: FSMContext, bot: Bot):
+    """
+    Получаем ввод пользователя ввод пользователя
+    :param message:
+    :param state:
+    :param bot:
+    :return:
+    """
+    logging.info(f'get_input_other: {message.chat.id}')
+    await state.set_state(state=None)
+    # await message.answer(text='Отлично ',
+    #                      reply_markup=ReplyKeyboardRemove())
+    await message.answer(text='Отлично ',
+                         reply_markup=kb.keyboards_start_user())
+    # await bot.delete_message(chat_id=message.chat.id,
+    #                          message_id=message.message_id + 1)
+    # await bot.delete_message(chat_id=message.chat.id,
+    #                          message_id=message.message_id + 2)
+    data = await state.get_data()
+    if data['type'] == 'land':
+        await state.update_data(district=message.text)
+        await message.answer(text=f'Какой площади участок вы рассматриваете?',
+                             reply_markup=kb.keyboard_type_land_area())
+    elif data['type'] == 'house':
+        await state.update_data(district=message.text)
+        await message.answer(text=f'Тип материала дома?',
+                             reply_markup=kb.keyboard_type_wall())
+
+
+@router.callback_query(F.data.startswith('land_area'))
+@error_handler
+async def get_land_area(callback: CallbackQuery, state: FSMContext, bot: Bot):
+    """
+    Получаем площадь участка
+    :param callback:
+    :param state:
+    :param bot:
+    :return:
+    """
+    logging.info(f'get_land_area {callback.message.chat.id}')
+    answer = callback.data.split('_')[-1]
+    await state.update_data(land_area=answer)
+    await callback.message.edit_text(text='Какой вариант оплаты будете использовать?',
+                                     reply_markup=kb.keyboard_payment_option())
+    await callback.answer()
+
+
+@router.callback_query(F.data.startswith('payment_option'))
+@error_handler
+async def get_payment_option(callback: CallbackQuery, state: FSMContext, bot: Bot):
+    """
+    Получаем способ оплаты
+    :param callback:
+    :param state:
+    :param bot:
+    :return:
+    """
+    logging.info(f'get_payment_option {callback.message.chat.id}')
+    answer = callback.data.split('_')[-1]
+    data = await state.get_data()
+    if data['type'] == 'land':
+        await state.update_data(payment_option=answer)
+        await callback.message.edit_text(text='В какие сроки готовы приобретать участок?',
+                                         reply_markup=kb.keyboard_deadline())
+    elif data['type'] == 'house':
+        await state.update_data(payment_option=answer)
+        await callback.message.edit_text(text='В какие сроки готовы выйти на сделку?',
+                                         reply_markup=kb.keyboard_deadline_house())
+    await callback.answer()
+
+
+@router.callback_query(F.data.startswith('deadline'))
+@error_handler
+async def get_deadline(callback: CallbackQuery, state: FSMContext, bot: Bot):
+    """
+    Получаем срок выхода на сделку
+    :param callback:
+    :param state:
+    :param bot:
+    :return:
+    """
+    logging.info(f'get_deadline {callback.message.chat.id}')
+    answer = callback.data.split('_')[-1]
+    await state.update_data(deadline=answer)
+    await state.set_state(User.phone)
+    await bot.delete_message(chat_id=callback.message.chat.id,
+                             message_id=callback.message.message_id)
+    await callback.message.answer(text=f'Оставьте номер телефона наш специалист Вам перезвонит',
+                                  reply_markup=kb.keyboard_phone())
+
+
+
+    # await callback.message.answer(text='Благодарим вас за ответы, менеджер свяжется с вами в ближайшее время')
+    # await callback.answer()
+    # data = await state.get_data()
+    # text = ''
+    # if data["type"] == "land":
+    #     text = f'<b>Новая заявка:</b>\n\n' \
+    #            f'<i>Пользователь:</i> @{callback.from_user.username if callback.from_user.username else "Ник не указан"}/{callback.message.chat.id}\n' \
+    #            f'<i>Тип заявки:</i> {"Покупка" if data["payment"] == "bay" else "Продажа"}\n' \
+    #            f'<i>Тип недвижимости:</i> {data["type_e"]}\n' \
+    #            f'<i>Направление:</i> {data["district"]}\n' \
+    #            f'<i>Площадь участка:</i> {data["land_area"]} соток\n' \
+    #            f'<i>Способ оплаты:</i> {data["payment_option"]}\n' \
+    #            f'<i>Срок покупки:</i> {data["deadline"]}\n'
+    # elif data["type"] == "house":
+    #     text = f'<b>Новая заявка:</b>\n\n' \
+    #            f'<i>Пользователь:</i> @{callback.from_user.username if callback.from_user.username else "Ник не указан"}/{callback.message.chat.id}\n' \
+    #            f'<i>Тип заявки:</i> {"Покупка" if data["payment"] == "bay" else "Продажа"}\n' \
+    #            f'<i>Тип недвижимости:</i> {data["type_e"]}\n' \
+    #            f'<i>Есть участок?:</i> {data["land"]}\n' \
+    #            f'<i>Направление:</i> {data["district"]}\n' \
+    #            f'<i>Тип стен:</i> {data["type_wall"]}\n' \
+    #            f'<i>Бюджет:</i> {data["budget"]}\n' \
+    #            f'<i>Способ оплаты:</i> {data["payment_option"]}\n' \
+    #            f'<i>Срок покупки:</i> {data["deadline"]}\n'
+    # await bot.send_message(chat_id=config.tg_bot.channel,
+    #                        text=text)
+
+
+@router.callback_query(F.data.startswith('type_wall'))
+@error_handler
+async def get_payment_option(callback: CallbackQuery, state: FSMContext, bot: Bot):
+    """
+    Получаем тип стен
+    :param callback:
+    :param state:
+    :param bot:
+    :return:
+    """
+    logging.info(f'get_payment_option {callback.message.chat.id}')
+    answer = callback.data.split('_')[-1]
+    if answer == 'other':
+        await callback.message.edit_text(text='Укажите тип материала дома?')
+        await state.set_state(User.type_wall)
+        return
+    await state.update_data(type_wall=answer)
+    await callback.message.edit_text(text='Выберите бюджет строительства, руб.',
+                                     reply_markup=kb.keyboard_budget())
+    await callback.answer()
+
+
+@router.message(StateFilter(User.type_wall))
+@error_handler
+async def get_type_wall_other(message: Message, state: FSMContext, bot: Bot):
+    """
+    Получаем тип стен от пользователя
+    :param message:
+    :param state:
+    :param bot:
+    :return:
+    """
+    logging.info(f'get_payment_option {message.chat.id}')
+    await state.update_data(type_wall=message.text)
+    await state.set_state(state=None)
+    await message.answer(text='Выберите бюджет строительства, руб.',
+                         reply_markup=kb.keyboard_budget())
+
+
+@router.callback_query(F.data.startswith('budget'))
+@error_handler
+async def get_payment_option(callback: CallbackQuery, state: FSMContext, bot: Bot):
+    """
+    Получаем бюджет
+    :param callback:
+    :param state:
+    :param bot:
+    :return:
+    """
+    logging.info(f'get_payment_option {callback.message.chat.id}')
+    answer = callback.data.split('_')[-1]
+    await state.update_data(budget=answer)
+    await callback.message.edit_text(text='Какой вариант оплаты будете использовать?',
+                                     reply_markup=kb.keyboard_payment_option())
+    await callback.answer()
+
+
+@router.message(or_f(F.text, F.contact), StateFilter(User.phone))
+@error_handler
+async def process_validate_russian_phone_number(message: Message, state: FSMContext, bot: Bot) -> None:
+    """Получаем номер телефона пользователя (проводим его валидацию). Подтверждаем введенные данные"""
+    logging.info("process_start_command_user")
+    if message.contact:
+        phone = str(message.contact.phone_number)
+    else:
+        phone = message.text
+        if not validate_russian_phone_number(phone):
+            await message.answer(text="Неверный формат номера. Повторите ввод, например 89991112222:")
+            return
+    await state.update_data(phone=phone)
+    await message.answer(text='Благодарим вас за ответы, менеджер свяжется с вами в ближайшее время',
+                         reply_markup=kb.keyboards_start_user())
+    data = await state.get_data()
+    text = ''
+    if data["type"] == "flat":
+        text = f'<b>Новая заявка:</b>\n\n' \
+               f'<i>Пользователь:</i> @{message.from_user.username if message.from_user.username else "Ник не указан"}/{message.chat.id}\n' \
+               f'<i>Тип заявки:</i> {"Покупка" if data["payment"] == "bay" else "Продажа"}\n' \
+               f'<i>Тип недвижимости:</i> {data["type_e"]}\n' \
+               f'<i>Номер телефона:</i> {data["phone"]}'
+    elif data["type"] == "land":
+        text = f'<b>Новая заявка:</b>\n\n' \
+               f'<i>Пользователь:</i> @{message.from_user.username if message.from_user.username else "Ник не указан"}/{message.chat.id}\n' \
+               f'<i>Номер телефона:</i> {data["phone"]}\n' \
+               f'<i>Тип заявки:</i> {"Покупка" if data["payment"] == "bay" else "Продажа"}\n' \
+               f'<i>Тип недвижимости:</i> {data["type_e"]}\n' \
+               f'<i>Направление:</i> {data["district"]}\n' \
+               f'<i>Площадь участка:</i> {data["land_area"]} соток\n' \
+               f'<i>Способ оплаты:</i> {data["payment_option"]}\n' \
+               f'<i>Срок покупки:</i> {data["deadline"]}\n'
+    elif data["type"] == "house":
+        text = f'<b>Новая заявка:</b>\n\n' \
+               f'<i>Пользователь:</i> @{message.from_user.username if message.from_user.username else "Ник не указан"}/{message.chat.id}\n' \
+               f'<i>Номер телефона:</i> {data["phone"]}\n' \
+               f'<i>Тип заявки:</i> {"Покупка" if data["payment"] == "bay" else "Продажа"}\n' \
+               f'<i>Тип недвижимости:</i> {data["type_e"]}\n' \
+               f'<i>Есть участок?:</i> {data["land"]}\n' \
+               f'<i>Направление:</i> {data["district"]}\n' \
+               f'<i>Тип стен:</i> {data["type_wall"]}\n' \
+               f'<i>Бюджет:</i> {data["budget"]}\n' \
+               f'<i>Способ оплаты:</i> {data["payment_option"]}\n' \
+               f'<i>Срок покупки:</i> {data["deadline"]}\n'
+    await bot.send_message(chat_id=config.tg_bot.channel,
+                           text=text)
+
---
+++ keyboards/user_keyboards.py
@@ -0,0 +1,121 @@
+from aiogram.types import KeyboardButton, ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+import logging
+
+
+def keyboards_start_user() -> ReplyKeyboardMarkup:
+    logging.info("keyboards_start_user")
+    button_1 = KeyboardButton(text='🏠 Главное меню')
+    keyboard = ReplyKeyboardMarkup(
+        keyboard=[[button_1], ],
+        resize_keyboard=True
+    )
+    return keyboard
+
+
+def keyboard_bay_sell() -> InlineKeyboardMarkup:
+    logging.info("keyboard_bay_sell")
+    button_1 = InlineKeyboardButton(text='Купить',  callback_data=f'estate_bay')
+    button_2 = InlineKeyboardButton(text='Продать', callback_data=f'estate_sell')
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[[button_1], [button_2]], )
+    return keyboard
+
+
+def keyboard_type_estate_real() -> InlineKeyboardMarkup:
+    logging.info("keyboard_type_estate_real")
+    button_1 = InlineKeyboardButton(text='Земельный участок',  callback_data=f'type_estate_land')
+    button_2 = InlineKeyboardButton(text='Дом', callback_data=f'type_estate_house')
+    button_3 = InlineKeyboardButton(text='Квартиру', callback_data=f'type_estate_flat')
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[[button_1], [button_2], [button_3]], )
+    return keyboard
+
+
+def keyboard_type_land() -> ReplyKeyboardMarkup:
+    logging.info("keyboard_type_land")
+    button_1 = KeyboardButton(text='Север (Выборгский, Приозерский, Всеволожский р-н)')
+    button_2 = KeyboardButton(text='ЮГ (Гатчинский, Ломоносовский, Тосненский, Красносельский)')
+    button_3 = KeyboardButton(text='Юго-восток (Кировский, Всеволожский)')
+    button_4 = KeyboardButton(text='Только в границе СПБ')
+    button_5 = KeyboardButton(text='Другой вариант')
+    button_6 = KeyboardButton(text='🏠 Главное меню')
+    keyboard = ReplyKeyboardMarkup(
+        keyboard=[[button_1], [button_2], [button_3], [button_4], [button_5], [button_6]],
+        resize_keyboard=True
+    )
+    return keyboard
+
+
+def keyboard_type_estate_house() -> InlineKeyboardMarkup:
+    logging.info("keyboard_type_estate_house")
+    button_1 = InlineKeyboardButton(text='Да',  callback_data=f'type_house_Да')
+    button_2 = InlineKeyboardButton(text='Нет', callback_data=f'type_house_Нет')
+    button_3 = InlineKeyboardButton(text='В процессе приобретения', callback_data=f'type_house_в процессе приобретения')
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[[button_1], [button_2], [button_3]], )
+    return keyboard
+
+
+def keyboard_phone() -> ReplyKeyboardMarkup:
+    logging.info(f'keyboard_phone')
+    button_1 = KeyboardButton(text='Поделиться ☎️', request_contact=True)
+    button_2 = KeyboardButton(text='🏠 Главное меню')
+    keyboard = ReplyKeyboardMarkup(
+        keyboard=[[button_1], [button_2]], resize_keyboard=True
+    )
+    return keyboard
+
+
+def keyboard_type_land_area() -> InlineKeyboardMarkup:
+    logging.info("keyboard_type_land_area")
+    button_1 = InlineKeyboardButton(text='6 - 8 соток',  callback_data=f'land_area_6-8')
+    button_2 = InlineKeyboardButton(text='8 - 12 соток',  callback_data=f'land_area_8-12')
+    button_3 = InlineKeyboardButton(text='12 - 15 соток',  callback_data=f'land_area_12-15')
+    button_4 = InlineKeyboardButton(text='от 15 соток', callback_data=f'land_area_от 15')
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[[button_1], [button_2], [button_3], [button_4]])
+    return keyboard
+
+
+def keyboard_type_wall() -> InlineKeyboardMarkup:
+    logging.info("keyboard_type_wall")
+    button_1 = InlineKeyboardButton(text='Каменный',  callback_data=f'type_wall_Каменный')
+    button_2 = InlineKeyboardButton(text='Деревянный',  callback_data=f'type_wall_Деревянный')
+    button_3 = InlineKeyboardButton(text='Каркасный',  callback_data=f'type_wall_Каркасный')
+    button_4 = InlineKeyboardButton(text='Другое', callback_data=f'type_wall_other')
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[[button_1], [button_2], [button_3], [button_4]])
+    return keyboard
+
+
+def keyboard_budget() -> InlineKeyboardMarkup:
+    logging.info("keyboard_budget")
+    button_1 = InlineKeyboardButton(text='от 7 500 000 до 10 000 000',  callback_data=f'budget_от 7 500 000 до 10 000 000')
+    button_2 = InlineKeyboardButton(text='от 10 000 000 до 15 000 000',  callback_data=f'budget_от 10 000 000 до 15 000 000')
+    button_3 = InlineKeyboardButton(text='от 15 000 000',  callback_data=f'budget_от 15 000 000')
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[[button_1], [button_2], [button_3]])
+    return keyboard
+
+
+def keyboard_payment_option() -> InlineKeyboardMarkup:
+    logging.info("keyboard_type_type_wall")
+    button_1 = InlineKeyboardButton(text='Ипотека',  callback_data=f'payment_option_Ипотека')
+    button_2 = InlineKeyboardButton(text='Наличные',  callback_data=f'payment_option_Наличные')
+    button_3 = InlineKeyboardButton(text='Встречная продажа',  callback_data=f'payment_option_Встречная продажа')
+    button_4 = InlineKeyboardButton(text='Сертификат', callback_data=f'payment_option_Сертификат')
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[[button_1], [button_2], [button_3], [button_4]])
+    return keyboard
+
+
+def keyboard_deadline() -> InlineKeyboardMarkup:
+    logging.info("keyboard_deadline")
+    button_1 = InlineKeyboardButton(text='Сейчас',  callback_data=f'deadline_сейчас')
+    button_2 = InlineKeyboardButton(text='1-3 месяца',  callback_data=f'deadline_1-3 месяца')
+    button_3 = InlineKeyboardButton(text='от 3 месяцев',  callback_data=f'deadline_от 3 месяцев')
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[[button_1], [button_2], [button_3]])
+    return keyboard
+
+
+def keyboard_deadline_house() -> InlineKeyboardMarkup:
+    logging.info("keyboard_deadline")
+    button_1 = InlineKeyboardButton(text='Сейчас',  callback_data=f'deadline_сейчас')
+    button_2 = InlineKeyboardButton(text='1-3 месяца',  callback_data=f'deadline_1-3 месяца')
+    button_3 = InlineKeyboardButton(text='от 3 до 6 месяцев',  callback_data=f'deadline_от 3 до 6 месяцев')
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[[button_1], [button_2], [button_3]])
+    return keyboard
